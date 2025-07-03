5f26d23b409f7c693199c68faf63c4cce4c5df36
whezley <francisco.w.m.q@gmail.com>
2025-07-03 20:14:36 +0000

    declarando os atributos da carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,8 +9,16 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    char codigodaCidade[15];
+    char nomedaCidade[20];
+    int populacao;
+    float area;
+    float pib;
+    int numPontosTuristicos;
     // Cadastro das Cartas:
+
+
+    
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
